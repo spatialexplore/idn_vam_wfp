45655579e2ed5c0e8cdc1754bf0e58695a7f7f25
rgohagan <spatialexplore@gmail.com>
2015-10-13 14:22:36 +0700

    Added longTermAverage utility functions

---
+++ python/longTermAverage.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+__author__ = 'rochelle'
+
+# Import system modules
+from os import listdir, path
+import arcpy
+
+def calcAverage(fileList, avgFile):
+    print "calcAverage: ", fileList
+    arcpy.cellSize = "MAXOF"
+    arcpy.extent = "MAXOF"
+    outRaster = arcpy.sa.CellStatistics(fileList, "MEAN")
+    # Save the output
+    outRaster.save(avgFile)
+    print "saved avg in: ", avgFile
+    return 0
+
+def calcMin(fileList, minFile):
+    print "calcAverage: ", fileList
+    arcpy.cellSize = "MAXOF"
+    arcpy.extent = "MAXOF"
+    outRaster = arcpy.sa.CellStatistics(fileList, "MINIMUM")
+    # Save the output
+    outRaster.save(minFile)
+    print "saved minimum in: ", minFile
+    return 0
+
+def calcMax(fileList, maxFile):
+    print "calcAverage: ", fileList
+    arcpy.cellSize = "MAXOF"
+    arcpy.extent = "MAXOF"
+    outRaster = arcpy.sa.CellStatistics(fileList, "MAXIMUM")
+    # Save the output
+    outRaster.save(maxFile)
+    print "saved minimum in: ", maxFile
+    return 0
+
+def calcStDev(fileList, sdFile):
+    print "calcAverage: ", fileList
+    arcpy.cellSize = "MAXOF"
+    arcpy.extent = "MAXOF"
+    outRaster = arcpy.sa.CellStatistics(fileList, "STD")
+    # Save the output
+    outRaster.save(sdFile)
+    print "saved minimum in: ", sdFile
+    return 0
+
