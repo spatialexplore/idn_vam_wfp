2e99df13c6e21319b3625969412dd5cea2cb3554
rohagan <rochelle.ohagan@gmail.com>
2016-05-24 15:13:40 +0700

    Added function to get all files matching a pattern. Modified unzipFiles to use 'with' when opening files.

--- python/utilities/directoryUtils.py
+++ python/utilities/directoryUtils.py
@@ -32,46 +32,84 @@ VERSION
 """
 __author__ = 'rochelle'
 
-from os import listdir, path
+import os
+#from os import listdir, path
 import gzip
 import re
 
+def listMatchingFiles(base_dir, pattern):
+    # build list of files matching pattern
+    fileList = []
+    if not pattern:
+        pattern = '.*'
+    if os.path.exists(base_dir):
+        _all_files = os.listdir(base_dir)
+        for _fn in _all_files:
+            _pth = os.path.join(base_dir, _fn)
+            if not os.path.isdir(_pth):
+                if re.match(pattern, _fn):
+                    fileList.append(_pth)
+    return fileList
+
 def buildFileList(base_dir, extension='.tif'):
     fileList = []
-    if path.exists(base_dir):
-        all_files = listdir(base_dir)
+    if os.path.exists(base_dir):
+        all_files = os.listdir(base_dir)
         for fname in all_files:
-            pth = path.join(base_dir, fname)
-            if path.isdir(pth):
+            pth = os.path.join(base_dir, fname)
+            if os.path.isdir(pth):
                 # skip directories
                 continue
             else:
                 # file
-                fn, ext = path.splitext(fname)
+                fn, ext = os.path.splitext(fname)
                 if ext == extension:
                     fileList.append(pth)
     return fileList
 
 def getMatchingFiles(base_dir, filter):
     fileList = []
-    if path.exists(base_dir):
-        all_files = listdir(base_dir)
+    if os.path.exists(base_dir):
+        all_files = os.listdir(base_dir)
         for f in all_files:
-            pth = path.join(base_dir, f)
-            if not path.isdir(pth):
+            pth = os.path.join(base_dir, f)
+            if not os.path.isdir(pth):
                 # check file against filter
                 if re.match(filter, f):
                     fileList.append(pth)
     return fileList
 
+def getNewFilename(filename, prefix, ext, pattern, new_pattern):
+
+    f= os.path.basename(filename)
+    m = re.match(pattern, f)
+    new_filename = re.sub(pattern,
+                         lambda match: new_pattern.format(
+                             prefix = prefix,
+                             datestamp = match.group('datestamp'),
+                             ext = ext)
+                         if match.group('datestamp') else f, f)
+
+    return os.path.join(os.path.dirname(filename), new_filename)
+
 def unzipFiles(base_dir, extension='.gz'):
     filesList = buildFileList(base_dir, extension)
     for fl in filesList:
-        inF = gzip.open(fl, 'rb')
-        outfilename = fl[:-3]
-        outF = open(outfilename, 'wb')
-        outF.write( inF.read() )
-        inF.close()
-        outF.close()
-
+        if not os.path.isdir(fl):
+            with gzip.open(fl, 'rb') as in_file:
+                s = in_file.read()
+            # Now store the uncompressed data
+            path_to_store = fl[:-3]  # remove the '.gz' from the filename
+            # store uncompressed file data from 's' variable
+            with open(path_to_store, 'wb') as f:
+                f.write(s)
     return 0
+
+def unzipFile(fname, extension='.gz'):
+    inF = gzip.open(fname, 'rb')
+    outfilename = fname[:-3]
+    outF = open(outfilename, 'wb')
+    outF.write( inF.read() )
+    inF.close()
+    outF.close()
+    return outfilename
