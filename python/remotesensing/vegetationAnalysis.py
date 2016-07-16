d53125ee80d4cf92fe93616c524035538e64cd36
rgohagan <rochelle.ohagan@gmail.com>
2016-07-16 13:31:05 +0700

    Added calcVCI_os.

--- python/remotesensing/vegetationAnalysis.py
+++ python/remotesensing/vegetationAnalysis.py
@@ -1,8 +1,10 @@
 __author__ = 'rochelle'
 
-import arcpy
+#import arcpy
 import gdal, osr
-
+import numpy as np
+import rasterio
+import urllib2
 
 def calcTCI(cur_filename, lta_max_filename, lta_min_filename, dst_filename):
     # calculate Temperature Condition Index
@@ -17,53 +19,33 @@ def calcTCI(cur_filename, lta_max_filename, lta_min_filename, dst_filename):
     dst.save(dst_filename)
     return 0
 
-def _getNoDataValue(rasterfn):
-    raster = gdal.Open(rasterfn)
-    band = raster.GetRasterBand(1)
-    return band.GetNoDataValue()
-
-
-def _raster2array(raster_file):
-    raster = gdal.Open(raster_file)
-    band = raster.GetRasterBand(1)
-    return band.ReadAsArray()
-
-def _array2raster(rasterfn,newRasterfn,array):
-    raster = gdal.Open(rasterfn)
-    geotransform = raster.GetGeoTransform()
-    originX = geotransform[0]
-    originY = geotransform[3]
-    pixelWidth = geotransform[1]
-    pixelHeight = geotransform[5]
-    cols = raster.RasterXSize
-    rows = raster.RasterYSize
-
-    driver = gdal.GetDriverByName('GTiff')
-    outRaster = driver.Create(newRasterfn, cols, rows, 1, gdal.GDT_Float32)
-    outRaster.SetGeoTransform((originX, pixelWidth, 0, originY, 0, pixelHeight))
-    outband = outRaster.GetRasterBand(1)
-    outband.WriteArray(array)
-    outRasterSRS = osr.SpatialReference()
-    outRasterSRS.ImportFromWkt(raster.GetProjectionRef())
-    outRaster.SetProjection(outRasterSRS.ExportToWkt())
-    outband.FlushCache()
-
 def calcTCI_os(cur_filename, lta_max_filename, lta_min_filename, dst_filename):
-    #  calculate Temperature Condition Index using open-source functionality
+    # arcpy-free version
+    # calculate Temperature Condition Index
     # TCI = 100 x (LST_max - LST)/(LST_max - LST_min)
-    cur_RasterArray = _raster2array(cur_filename)
-    cur_nodata = _getNoDataValue(cur_filename)
-    lta_max_RasterArray = _raster2array(lta_max_filename)
-    lta_max_nodata = _getNoDataValue(lta_max_filename)
-    lta_min_RasterArray = _raster2array(lta_min_filename)
-    lta_min_nodata = _getNoDataValue(lta_min_filename)
 
-    denominator = lta_max_RasterArray[lta_max_RasterArray != lta_max_nodata] - lta_min_RasterArray[lta_min_RasterArray != lta_min_nodata]
-    numerator = lta_max_RasterArray - cur_RasterArray
-    dst_f = numerator / denominator * 100.0
-    _array2raster(cur_filename, dst_filename, dst_f)
+    with rasterio.open(cur_filename) as cur_r:
+        cur_band = cur_r.read(1, masked=True)
+        profile = cur_r.profile.copy()
+        print cur_r.nodatavals
+        with rasterio.open(lta_max_filename) as lta_max_r:
+            lta_max_a = lta_max_r.read(1, masked=True)
+            with rasterio.open(lta_min_filename) as lta_min_r:
+                lta_min_a = lta_min_r.read(1, masked=True)
+                dst_f = np.zeros(cur_band.shape)
+                newd_f = np.ma.masked_where(np.ma.mask_or(np.ma.getmask(cur_band), np.ma.getmask(lta_max_a)),
+                                            dst_f)
+                numerator=(lta_max_a - cur_band)
+                denominator = (lta_max_a - lta_min_a)
+                newd_f += (np.divide(numerator, denominator) * 100.0)
+
+                res = newd_f.filled(fill_value=cur_r.nodata)
+                res2 = np.ma.masked_where(res == cur_r.nodata, res)
+
+                profile.update(dtype=rasterio.float64)
+                with rasterio.open(dst_filename, 'w', **profile) as dst:
+                    dst.write(res2.astype(rasterio.float64), 1)
     return 0
-
 
 def calcVCI(cur_filename, evi_max_filename, evi_min_filename, dst_filename):
      # calculate Vegetation Condition Index
@@ -81,6 +63,38 @@ def calcVCI(cur_filename, evi_max_filename, evi_min_filename, dst_filename):
     dst.save(dst_filename)
     return 0
 
+def calcVCI_os(cur_filename, evi_max_filename, evi_min_filename, dst_filename):
+    # arcpy-free version
+    # calculate Vegetation Condition Index
+    # VCI = 100 x (EVI - EVI_min)/(EVI_max - EVI_min)
+    with rasterio.open(cur_filename) as cur_r:
+        cur_band = cur_r.read(1, masked=True)
+        profile = cur_r.profile.copy()
+        print cur_r.nodatavals
+        with rasterio.open(evi_max_filename) as evi_max_r:
+#            evi_max_a = evi_max_r.read(1, masked=True)
+            evi_max_w = evi_max_r.read(1, window=((0, cur_band.shape[0]), (0, cur_band.shape[1])), masked=True)
+            with rasterio.open(evi_min_filename) as evi_min_r:
+#                evi_min_a = evi_min_r.read(1, masked=True)
+                evi_min_w = evi_min_r.read(1, window=((0, cur_band.shape[0]), (0, cur_band.shape[1])), masked=True)
+                dst_f = np.zeros(cur_band.shape)
+                newd_f = np.ma.masked_where(np.ma.getmask(cur_band),
+                                            dst_f)
+#                evi_min_ma = np.ma.masked_where(np.ma.getmask(cur_band), evi_min_w)
+                # newd_f = np.ma.masked_where(np.ma.mask_or(np.ma.getmask(cur_band), np.ma.getmask(evi_max_a)),
+                #                             dst_f)
+                numerator = (cur_band - evi_min_w)
+                denominator = (evi_max_w - evi_min_w)
+                with np.errstate(divide='ignore'):
+                    newd_f += (np.divide(numerator, denominator) * 100.0)
+                res = newd_f.filled(fill_value=cur_r.nodata)
+                res2 = np.ma.masked_where(res == cur_r.nodata, res)
+
+                profile.update(dtype=rasterio.float64)
+                with rasterio.open(dst_filename, 'w', **profile) as dst:
+                    dst.write(res2.astype(rasterio.float64), 1)
+    return 0
+
 def calcVHI(vci_filename, tci_filename, dst_filename):
      # calculate Vegetation Health Index
     # VHI = 0.5 x (VCI + TCI)
@@ -90,4 +104,25 @@ def calcVHI(vci_filename, tci_filename, dst_filename):
     dst_f = arcpy.sa.Plus(vci_Raster, tci_Raster)
     dst = arcpy.sa.Times(dst_f, 0.5)
     dst.save(dst_filename)
+    return 0
+
+def calcVHI_os(vci_filename, tci_filename, dst_filename):
+    # arcpy-free version
+    # calculate Vegetation Health Index
+    # VHI = 0.5 x (VCI + TCI)
+    with rasterio.open(vci_filename) as vci_r:
+        vci_a = vci_r.read(1, masked=True)
+        profile = vci_r.profile.copy()
+        print vci_r.nodatavals
+        with rasterio.open(tci_filename) as tci_r:
+            tci_a = tci_r.read(1, masked=True)
+            dst_f = np.zeros(tci_a.shape)
+            newd_f = np.ma.masked_where(np.ma.mask_or(np.ma.getmask(vci_a), np.ma.getmask(tci_a)),
+                                        dst_f)
+            newd_f += ((vci_a + tci_a) * 0.5)
+            res = newd_f.filled(fill_value=vci_r.nodata)
+            res2 = np.ma.masked_where(res == vci_r.nodata, res)
+            profile.update(dtype=rasterio.float64)
+            with rasterio.open(dst_filename, 'w', **profile) as dst:
+                dst.write(res2.astype(rasterio.float64), 1)
     return 0
