a79f07ca02756e5c43ef45dfb85614c32ce17f00
rohagan <rochelle.ohagan@gmail.com>
2016-07-16 12:37:23 +0700

    Added support for open-source (linux). Replicated functions as XX_os. Uses rasterio and numpy.

--- python/remotesensing/longTermAverage.py
+++ python/remotesensing/longTermAverage.py
@@ -4,7 +4,9 @@ __author__ = 'rochelle'
 
 # Import system modules
 from os import listdir, path
-import arcpy
+#import arcpy
+import rasterio
+import numpy as np
 
 def calcAverage(fileList, avgFile):
     print "calcAverage: ", fileList
@@ -16,8 +18,27 @@ def calcAverage(fileList, avgFile):
     print "saved avg in: ", avgFile
     return 0
 
-def calcMin(fileList, minFile):
+def calcAverage_os(fileList, avgFile):
     print "calcAverage: ", fileList
+    if fileList:
+        arrayList = []
+        first = True
+        profile = None
+        for f in fileList:
+            with rasterio.open(f) as cur_r:
+                if first:
+                    profile = cur_r.profile.copy()
+                    first = False
+                cur_a = cur_r.read(1, masked=True)
+                arrayList.append(cur_a)
+        dst_a = np.vstack(arrayList)
+        dst_r = np.mean(dst_a, axis=0)
+        with rasterio.open(avgFile, 'w', **profile) as dst:
+            dst.write(dst_r.astype(rasterio.float64), 1)
+
+
+def calcMin(fileList, minFile):
+    print "calcMin: ", fileList
     arcpy.cellSize = "MAXOF"
     arcpy.extent = "MAXOF"
     outRaster = arcpy.sa.CellStatistics(fileList, "MINIMUM")
@@ -25,6 +46,27 @@ def calcMin(fileList, minFile):
     outRaster.save(minFile)
     print "saved minimum in: ", minFile
     return 0
+
+
+def calcMin_os(fileList, minFile):
+    print "calcMin (open source version): ", fileList
+    if fileList:
+        arrayList = []
+        first = True
+        profile = None
+        for f in fileList:
+            with rasterio.open(f) as cur_r:
+                if first:
+                    profile = cur_r.profile.copy()
+                    first = False
+                cur_a = cur_r.read(1, masked=True)
+                arrayList.append(cur_a)
+        dst_a = np.vstack(arrayList)
+        dst_r = np.amin(dst_a, axis=0)
+        with rasterio.open(minFile, 'w', **profile) as dst:
+            dst.write(dst_r.astype(rasterio.float64), 1)
+            print "saved minimum in: ", minFile
+
 
 def calcMax(fileList, maxFile):
     print "calcAverage: ", fileList
@@ -36,8 +78,28 @@ def calcMax(fileList, maxFile):
     print "saved maximum in: ", maxFile
     return 0
 
+def calcMax_os(fileList, maxFile):
+    print "calcMax (open source version): ", fileList
+    if fileList:
+        arrayList = []
+        first = True
+        profile = None
+        for f in fileList:
+            with rasterio.open(f) as cur_r:
+                if first:
+                    profile = cur_r.profile.copy()
+                    first = False
+                cur_a = cur_r.read(1, masked=True)
+                arrayList.append(cur_a)
+        dst_a = np.vstack(arrayList)
+        dst_r = np.amax(dst_a, axis=0)
+        with rasterio.open(maxFile, 'w', **profile) as dst:
+            dst.write(dst_r.astype(rasterio.float64), 1)
+            print "saved maximum in: ", maxFile
+
+
 def calcStDev(fileList, sdFile):
-    print "calcAverage: ", fileList
+    print "calcStDev: ", fileList
     arcpy.cellSize = "MAXOF"
     arcpy.extent = "MAXOF"
     outRaster = arcpy.sa.CellStatistics(fileList, "STD")
@@ -45,6 +107,25 @@ def calcStDev(fileList, sdFile):
     outRaster.save(sdFile)
     print "saved standard deviation in: ", sdFile
     return 0
+
+def calcStDev_os(fileList, sdFile):
+    print "calcStDev (open source version): ", fileList
+    if fileList:
+        arrayList = []
+        first = True
+        profile = None
+        for f in fileList:
+            with rasterio.open(f) as cur_r:
+                if first:
+                    profile = cur_r.profile.copy()
+                    first = False
+                cur_a = cur_r.read(1, masked=True)
+                arrayList.append(cur_a)
+        dst_a = np.vstack(arrayList)
+        dst_r = np.std(dst_a, axis=0, ddof=1)
+        with rasterio.open(sdFile, 'w', **profile) as dst:
+            dst.write(dst_r.astype(rasterio.float64), 1)
+            print "saved standard deviation in: ", sdFile
 
 def calcSum(fileList, sdFile):
     print "calcSum: ", fileList
@@ -55,3 +136,23 @@ def calcSum(fileList, sdFile):
     outRaster.save(sdFile)
     print "saved sum in: ", sdFile
     return 0
+
+def calcSum_os(fileList, sdFile):
+    print "calcSum (open source version): ", fileList
+    if fileList:
+        arrayList = []
+        first = True
+        profile = None
+        for f in fileList:
+            with rasterio.open(f) as cur_r:
+                if first:
+                    profile = cur_r.profile.copy()
+                    first = False
+                cur_a = cur_r.read(1, masked=True)
+                arrayList.append(cur_a)
+        dst_a = np.vstack(arrayList)
+        dst_r = np.sum(dst_a, axis=0, ddof=1)
+        with rasterio.open(sdFile, 'w', **profile) as dst:
+            dst.write(dst_r.astype(rasterio.float64), 1)
+            print "saved standard deviation in: ", sdFile
+
